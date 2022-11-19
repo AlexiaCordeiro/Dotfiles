18cc775d21bb31b959f1382eb92517586801b139
AlexiaCordeiro <alexiacordeiro@tutanota.com>
2022-11-18 22:50:45 -0300

    corrected an sintaxe error on bluetoothctl

--- bluetoothctl.sh
+++ bluetoothctl.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-yybluetoothctl remove B8:F6:53:F5:F9:E0
+bluetoothctl remove B8:F6:53:F5:F9:E0
 notify-send "Please make sure Headphone is in pairing mode."
 timeout 15s bluetoothctl scan on
+bluetoothctl pair B8:F6:53:F5:F9:E0
 bluetoothctl connect B8:F6:53:F5:F9:E0
 if [[ $(bluetoothctl connect B8:F6:53:F5:F9:E0) = *'Device B8:F6:53:F5:F9:E0 not available'* ]]; then
-  notify-send "bluetooth mouse not detected. Make sure your device is in pairing mode. Trying again in 30 seconds..."
+  notify-send "bluetooth headphone not detected. Make sure your device is in pairing mode. Trying again in 30 seconds..."
   timeout 30s bluetoothctl scan on
   bluetoothctl connect B8:F6:53:F5:F9:E0
   if [[ $(bluetoothctl connect B8:F6:53:F5:F9:E0) = *'Device B8:F6:53:F5:F9:E0 not available'* ]]; then
-    notify-send "bluetooth mouse not detected. Make sure your device is in pairing mode. Trying again in 30 seconds..."
+    notify-send "bluetooth headphone not detected. Make sure your device is in pairing mode. Trying again in 30 seconds..."
     timeout 30s bluetoothctl scan on
     bluetoothctl connect B8:F6:53:F5:F9:E0
     if [[ $(bluetoothctl connect B8:F6:53:F5:F9:E0) = *'Device B8:F6:53:F5:F9:E0 not available'* ]]; then
-        notify-send "bluetooth mouse paring unsuccesful. Please try again later."
+        notify-send "bluetooth headphone paring unsuccesful. Please try again later."
     else
         notify-send "Pairing successful. Connected to Headphone."
     fi
